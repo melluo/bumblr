7b2a8e06fad78c655be1c610490586469be2494f
Melody <luomel@outlook.com>
2020-02-05 22:01:23 -0500

    Added testinglibraries

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,13 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  RSpec.configure do |config|
+    config.include FactoryBot::Syntax::Methods
+  end
+end
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
