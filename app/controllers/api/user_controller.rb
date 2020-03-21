71dc1dd39dc6b32433eafe46899197a6024fc096
melluo <luomel@outlook.com>
2020-03-21 00:26:46 -0400

    finish frontend user auth

---
+++ app/controllers/api/user_controller.rb
@@ -0,0 +1,24 @@
+class UserController < ApplicationController
+    def create
+        @user = User.new(user_params)
+        if @user.save 
+            login!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def index  #index is showing all users
+        render :index
+    end
+    
+    def show 
+        @user = User.find(params[:id])
+        render :show
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password, :email)
+    end
+end
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+
+module.exports = {
+    entry: './frontend/bumblr.jsx',
+    output: {
+        path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.jsx?$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }, 
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '*'],
+        modules: [
+            'node_modules'
+        ]
+    }
+};
